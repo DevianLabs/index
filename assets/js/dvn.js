090a4b4ff869c6a8a0ff87a9335ec46b8a1910f4
Miguel Salazar <mig.salazars@gmail.com>
2016-07-20 02:02:05 -0500

    oops

--- assets/js/dvn.js
+++ assets/js/dvn.js
@@ -17,20 +17,7 @@
 
 			//callback
 			onTyped: function() {
-          var param = {
-            flames : [
-              { x: 0, hsla: [300, 100, 80, .1], y: 0, blur: .1 },
-              { x: 0, hsla: [290, 100, 80, .8], y: .02, blur: .15 },
-              { x: 0, hsla: [280, 100, 80, .7], y: .05, blur: .2 },
-              { x: 0, hsla: [260, 100, 80, .6], y: .1, blur: .25 },
-              { x: 0, hsla: [240, 100, 80, .5], y: .10, blur: .3 },
-              { x: 0, hsla: [220, 100, 80, .4], y: .20, blur: .4 },
-              { x: 0, hsla: [200, 100, 80, .3], y: .1, blur: .5}
-            ]
-          };
-
-          var el = $('#dvn-text span')[0];
-          $(el).burn(param);
+          
       }
 		};
 
