2de243d5a7f1fb766b4494ce2ede3fea1c6febee
Mig Salazar <migsalazar@users.noreply.github.com>
2016-07-19 21:03:39 -0500

    Change because plugin

--- assets/js/dvn.js
+++ assets/js/dvn.js
@@ -1,31 +1,27 @@
 (function($, window, document) {
 
    $(function() {
-     var  dvn = 'devian labs',
-          textpart = '';
-     var  arr = dvn.split(''),
-          timer;
-
-      function animate() {
-        if(arr.length > 0) {
-          textpart += arr.shift();
-          $('#dvn-text').html(textpart);
-        }
-        else {
-          clearTimeout(timer);
-          return false;
-        }
-
-        timer = setTimeout(function(){
-            animate();
-        }, 100);
-      }
-
-      $('#dvn-text').after('<label class="cursor">_</label>');
-      animate();
-
-      (function blink(){
-         $('.cursor').fadeOut(500).fadeIn(500, blink);
-      })();
+		
+		var params = {
+			//show cursor
+			cursor: true,
+			
+			//pipe, underscore, terminal
+			cursortype: 'underscore',
+			
+			//delay between words
+			delay: 200,
+			
+			//text type
+			text: ['Text 1'],
+			
+			//callback
+			//onTyped: function() { 
+				//nothing
+			//}
+		};
+		
+		$('#dvn-text').typian(params);
+		
    });
   }(window.jQuery, window, document));
